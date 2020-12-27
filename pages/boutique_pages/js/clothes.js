9ddc6ac705f76fedc1415540496b8f7ad42fc0ff
POTRA BOGDAN <potrry@gmail.com>
2020-12-27 22:25:41 +0200

    [potri] add clothes boutique

---
+++ pages/boutique_pages/js/clothes.js
@@ -0,0 +1,30 @@
+window.onload  = function () {
+    allBuyButtons = document.getElementsByClassName("buy_btn");
+    available_tokens = document.getElementById("available_tokens");
+
+    for(let i=0;i<allBuyButtons.length;i++)
+    {
+        currentBuyBtn = allBuyButtons[i]
+        currentBuyBtn.onclick = function () {
+            addBtnFunctionality(this,available_tokens)
+        }
+    }
+}
+
+function addBtnFunctionality(button, available_tokens){
+    tokens = parseInt(available_tokens.innerText.split(" ")[2])
+    price = parseInt(button.parentNode.childNodes[3].innerText.split(" ")[0])
+    if(tokens-price >= 0)
+    {
+        tokens = tokens - price;
+        available_tokens.innerText = "Available tokens: " + tokens
+
+        button.style.backgroundColor = "black"
+        button.innerText = "owned"
+        button.disabled = true
+    }
+    else
+    {
+        alert("You don't have enough tokens to buy that!")
+    }
+}
