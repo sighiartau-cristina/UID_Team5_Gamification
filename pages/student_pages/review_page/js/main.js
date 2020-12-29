a0296c75688d4050e1146a7e3b86d6bc185eabac
Cristina Sighiartau <andra_cristina98@yahoo.com>
2020-12-29 22:32:34 +0200

    [cris] student-quiz

---
+++ pages/student_pages/review_page/js/main.js
@@ -0,0 +1,23 @@
+function startReview(){
+    modal.style.display = "block";
+}
+
+var modal = document.getElementById("myModal");
+
+$(document).ready( function (){
+  
+  modal = document.getElementById("myModal");
+  var span = document.getElementsByClassName("close")[0];
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+});
