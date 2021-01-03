2852d6bfb6c10a9141608d65568deb77fe0b439d
Teodora Ana Farcas <teodora.farcas@office365.3pillarglobal.com>
2021-01-03 21:12:49 +0200

    [teo] secretary-appointment-pages

--- pages/student_pages/student_upload/js/main.js
+++ pages/student_pages/student_upload/js/main.js
@@ -2,35 +2,34 @@ var file=false;
 
 function allowDrop(ev) {
     ev.preventDefault();
-  }
+}
 
-  function drag(ev) {
+function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
-  }
+}
 
-  function drop(ev) {
+function drop(ev) {
     ev.preventDefault();
     file=true;
     $('#label').hide();
     $('#files').append("ASSIGNMENT FILE<br>")
-  }
+}
 
-  function openAttachment() {
+function openAttachment() {
     $('#attachment').click();
-  }
+}
 
-  function fileSelected(input){
+function fileSelected(input){
     file=true;
     $('#label').hide();
     $('#files').append("ASSIGNMENT FILE<br>")
+}
 
+function submitAssignment(){
+  if(!file){
+      alert("Please choose files first.")
   }
-
-  function submitAssignment(){
-      if(!file){
-          alert("Please choose files first.")
-      }
-      else{
-      window.location.href='../student_upload/submitted_assignment.html'
-    }
+  else{
+    window.location.href='../student_upload/submitted_assignment.html'
   }
+}
