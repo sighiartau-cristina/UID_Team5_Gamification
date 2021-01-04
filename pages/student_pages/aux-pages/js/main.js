bca8df443037c5006ac184aead0940bab2fe7cbc
Cristina Sighiartau <andra_cristina98@yahoo.com>
2021-01-04 16:27:53 +0200

    [cris] js for aux pages

---
+++ pages/student_pages/aux-pages/js/main.js
@@ -0,0 +1,7 @@
+function turnIn() {
+    window.location.href = '../student_upload/upload_assignment.html';
+}
+
+function startReview() {
+    window.location.href = '../review_page/review_page.html';
+}
